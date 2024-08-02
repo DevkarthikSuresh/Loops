533104e404f17ff914f6d25fcb21dc82de8631b1
DevkarthikSuresh <devkarthik300@gmail.com>
2024-08-02 11:31:19 +0530

    Update even_number_printing.cpp

---
+++ even_number_printing.cpp
@@ -0,0 +1,26 @@
+
+
+//Name: Devkarthik Suresh
+//PRN: 23070123045
+//AIM - To print even number using loops.
+#include <iostream>
+using namespace std;
+
+int main() {
+ for(int i=0;i<11;i++)
+{if(i%2==0)
+{cout << i<< endl;}}
+
+    return 0;
+}
+
+/*Output-
+0
+2
+4
+6
+8
+10
+
+
+*/
