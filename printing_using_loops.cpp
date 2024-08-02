0d966d04e8b7b62c748eadefab6568ba6273ca55
DevkarthikSuresh <devkarthik300@gmail.com>
2024-08-02 11:31:55 +0530

    Update printing_using_loops.cpp

---
+++ printing_using_loops.cpp
@@ -0,0 +1,24 @@
+//Name: Devkarthik Suresh
+//PRN: 23070123045
+//AIM - To print multiple times using loop
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { 
+for(int i=0;i<5;i++)
+
+{cout << "hello"<< endl;}
+
+    return 0;
+}
+
+/*Output-
+hello
+hello
+hello
+hello
+hello
+
+
+*/
