dd881c21b9010fabdeababd8479f0093c9671488
DevkarthikSuresh <devkarthik300@gmail.com>
2024-08-09 11:47:25 +0530

    Create hello_loop.cpp

---
+++ hello_loop.cpp
@@ -0,0 +1,25 @@
+//Name:Devkarthik Suresh
+//PRN:23070123045
+//Aim:Implemention of loops to print 'Hello' 5 times.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { 
+    string a;
+    a = "Hello";
+for(int i=0;i<5;i++)
+
+{cout << "Hello"<< endl;}
+
+    return 0;
+}
+
+
+/*Output:
+Hello
+Hello
+Hello
+Hello
+Hello*/
