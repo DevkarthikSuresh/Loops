900e2c6824a320524411d71f78ffe0d58ee884d8
DevkarthikSuresh <devkarthik300@gmail.com>
2024-08-09 11:52:30 +0530

    Create 1-20_printing.cpp

---
+++ 1-20_printing.cpp
@@ -0,0 +1,40 @@
+//Name:Devkarthik Suresh
+//PRN:23070123045
+//Aim:To print numbers from 1 to 20 using while loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int i=0;
+        while(i<20){
+            cout<<i+1<<" ";
+            i++;
+            cout<< endl ;
+        }
+}
+
+
+/*Output:
+1 
+2 
+3 
+4 
+5 
+6 
+7 
+8 
+9 
+10 
+11 
+12 
+13 
+14 
+15 
+16 
+17 
+18 
+19 
+20 
+*/
